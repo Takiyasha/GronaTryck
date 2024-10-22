93f087ea1855403a680287ed733fda5b44f9604f
Taki Yasha <v.promsrichan@hotmail.com>
2024-10-22 15:48:21 +0200

    Products.json Data Stored
    May need ajustment later

--- generateProducts.js
+++ generateProducts.js
@@ -1,9 +1,107 @@
+// use the node generateProducts.js to write to the server files(json)
 const fs = require("fs");
 const path = require("path");
 
-// Define the folder where your images are stored
 // Adjust this path to match the actual location of the images folder
 const imagesFolderPath = path.join(__dirname, "public", "images", "StSt AW24");
+
+// Predefined data for attributes
+const predefinedAttributes = {
+  "Bomber 2.0": {
+    colors: ["Black", "Blue", "Green"],
+    fit: ["Unisex"],
+  },
+  Brooker: {
+    colors: ["Green", "Black", "White"],
+    fit: ["Unisex"],
+  },
+  "Changer 2.0": {
+    colors: ["Blue", "White", "Red", "Green"],
+    fit: ["Unisex"],
+  },
+  "Connector 2.0": {
+    colors: ["Black", "Blue", "Red", "White"],
+    fit: ["Unisex"],
+  },
+  Crafter: {
+    colors: ["Blue", "Red", "Black", "White"],
+    fit: ["Herr"],
+  },
+  "Creator 2.0": {
+    colors: ["Blue", "Red", "White", "Green"],
+    fit: ["Unisex"],
+  },
+  "Cruiser 2.0": {
+    colors: ["Blue", "White", "Green", "Red"],
+    fit: ["Unisex"],
+  },
+  "Drummer 2.0": {
+    colors: ["Blue", "White", "Red"],
+    fit: ["Unisex"],
+  },
+  Freestyler: {
+    colors: ["Blue", "White", "Red", "Green", "Black", "Yellow"],
+    fit: ["Unisex"],
+  },
+  Knoxer: {
+    colors: ["Blue", "White", "Black", "Red"],
+    fit: ["Unisex"],
+  },
+  Liner: {
+    colors: ["Black", "White"],
+    fit: ["Unisex"],
+  },
+  Mixer: {
+    colors: ["Blue", "White", "Green", "Red", "Pink", "Black"],
+    fit: ["Unisex"],
+  },
+  "Prepster 2.0": {
+    colors: ["Blue", "White", "Red", "Green", "Yellow", "Pink", "Black"],
+    fit: ["Unisex"],
+  },
+  Puffer: {
+    colors: ["Blue", "Black"],
+    fit: ["Unisex"],
+  },
+  "Radder 2.0": {
+    colors: ["Blue", "White", "Black", "Green", "Red", "Yellow"],
+    fit: ["Unisex"],
+  },
+  Roller: {
+    colors: ["Blue", "Red", "Yellow"],
+    fit: ["Unisex"],
+  },
+  "Slammer 2.0": {
+    colors: ["Blue", "White", "Black", "Green", "Red", "Yellow"],
+    fit: ["Unisex"],
+  },
+  "Sparker 2.0": {
+    colors: ["Blue", "Red"],
+    fit: ["Unisex"],
+  },
+  Stanley: {
+    colors: ["Blue", "Black", "Red", "Yellow", "White", "Green"],
+    fit: ["Herr"],
+  },
+  Stella: {
+    colors: ["Blue", "White", "Black", "Green", "Red", "Yellow", "Pink"],
+    fit: ["Dam"],
+  },
+  Striker: {
+    colors: ["Blue", "White", "Black", "Red"],
+    fit: ["Unisex"],
+  },
+  Trekker: {
+    colors: ["Blue", "Yellow", "Black"],
+    fit: ["Unisex"],
+  },
+  "Trucker 2.0": {
+    colors: ["Blue", "Yellow", "Black", "White", "Red", "Green"],
+    fit: ["Unisex"],
+  },
+
+  // Add more products as needed
+};
 
 fs.readdir(imagesFolderPath, (err, files) => {
   if (err) {
@@ -12,12 +110,30 @@ fs.readdir(imagesFolderPath, (err, files) => {
   }
 
   // Generate products data
-  const products = files.map((file, index) => ({
-    id: index + 1,
-    name: path.basename(file, path.extname(file)), // Use the filename as the name
-    price: Math.floor(Math.random() * 1000) + 100, // Random price for example
-    image: `/static/images/StSt AW24/${file}`, // Adjusted to match the correct static path
-  }));
+  const products = files.map((file, index) => {
+    const fullName = path.basename(file, path.extname(file));
+
+    // Try to match a base name with the predefined attributes
+    const baseName =
+      Object.keys(predefinedAttributes).find((name) =>
+        fullName.includes(name)
+      ) || fullName.split("_")[0];
+
+    // Get the attributes for the matched base name
+    const attributes = predefinedAttributes[baseName] || {
+      colors: [],
+      fit: [],
+    };
+
+    return {
+      id: index + 1,
+      name: baseName,
+      price: Math.floor(Math.random() * 1000) + 100, // Random price for example
+      image: `/static/images/StSt AW24/${file}`, // Adjusted to match the correct static path
+      colors: attributes.colors,
+      fit: attributes.fit,
+    };
+  });
 
   // Save the products data to a JSON file
   fs.writeFile(
@@ -32,5 +148,3 @@ fs.readdir(imagesFolderPath, (err, files) => {
     }
   );
 });
-
-//use the node generateProducts.js to write to the server files(json)
